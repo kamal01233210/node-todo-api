c332abf78d74ac1b6f37ac6fa7eaf1be2af34c6c
kamal01233210 <sehrawatkamal@gmail.com>
2018-05-31 07:12:00 +0530

    second

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -20,7 +20,7 @@ mongoClient.connect('mongodb://localhost:27017',(err,client)=>{
 	});*/
 
 
-	
+	//insert document	
 	/*db.collection('Todos').find({
 		_id:new ObjectID('5b0cbd003185cc1b08b91246')
 	}).toArray().then((docs)=>{
@@ -29,12 +29,40 @@ mongoClient.connect('mongodb://localhost:27017',(err,client)=>{
 		console.log('Unable to fetch todos',err);
 	});*/
 
-	db.collection('Todos').count().then((count)=>{
+	//find document
+	/*db.collection('Todos').count().then((count)=>{
 		console.log(count);
 		//console.log(JSON.stringify(docs,undefined,2));
 	},(err)=>{
 		console.log('Unable to fetch todos',err);
-	});	
+	});	*/
 
+	//delete documnet
+	//deleteMany is used to delete many documnets
+	//deleteOne delete only single document
+	//findOneAndDelete deletes only one documnet but it also returns the document which gets deleted
+	/*db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+		console.log(result);
+	},(err)=>{
+
+	});*/
+
+	/*Update command  
+		update also has similiar commands to delete
+	*/
+	//findOneAndUpdate is used to update the document and retunrs the result
+
+	db.collection('Todos').findOneAndUpdate({
+		_id: new ObjectID('5b0f4d2fdd699334148a1daa')
+	},{
+		$set:{
+			completed:true
+		}
+	},{
+		//return original is used to whether return the original documnet or the updated one
+		returnOriginal:false
+	}).then((result)=>{
+		console.log(result);	
+	});
 	//client.close(); 
 });
